645746733830cc9d445966c09489a022a748c07e
xav <xavraspi@gmail.com>
2019-08-13 03:14:12 +0200

    ça cause avec

--- parle.sh
+++ parle.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 #
-python start.py
 
-Parle ()
-{
-	parle=$(echo "Gooodailleniooos")
-	wget -q -U Mozilla -O output.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q='RREUH HIHICHICHICHITITITOTITO $parle GONEZALESSE et et'&tl=Fr-fr"
+function Parle(){
+	local parle="Hello $1 eoTO hip hop $1 h11ip haaaaaaaaaaaaaaaaaaaaaaaahhhhhhhhhahhahaahahha tuupuuduucuulaamoorooïïdee $1 op et et Yioiyupla"
+	wget -q -U Mozilla -O output.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&total=1&idx=0&textlen=32&client=tw-ob&q='$parle'&tl=Fr-fr"
 	mplayer output.mp3
 }
 
 Parle
-
