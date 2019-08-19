bffc73d5658df39882bbb7b15ac87c6a8f900dac
xavraspi <xavraspi@gmail.com>
2019-08-19 12:27:31 +0200

    lecture mp3&wav Usage: pymixer.Mixer('./sons/YES.mp3')

---
+++ pymixer.py
@@ -0,0 +1,19 @@
+# https://github.com/xavraspi/bin.git
+# Usage: pymixer.Mixer()
+import time, sys
+import pygame
+
+def Mixer(son):
+    pygame.mixer.init()
+    pygame.mixer.music.load(son)
+    #pygame.mixer.Channel(2)
+    pygame.mixer.music.play()
+    time.sleep(3)
+
+#    volume = pygame.mixer.music.get_volume()
+#    print ("Son est Playing : ", isplaying, ", Volume: ", volume)
+#    exit(0)
+#print("done")
+
+Mixer('./sons/NO.mp3')
+
